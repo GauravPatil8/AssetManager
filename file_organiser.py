c9045469569c55dbd372b64e677e5d4f4e1d801b
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-01 15:36:32 +0530

    Add: Zip extraction and management

--- file_organiser.py
+++ file_organiser.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import bpy
-import threading
+import zipfile
 from Folder_namedb import create_and_populate
 from Folder_namedb import fetch_folder_name
 from Folder_namedb import update_folder_name
@@ -12,10 +12,14 @@ from Folder_namedb import psfn_updateName
 from Folder_namedb import table_inMemory
 from Folder_namedb import insert_inMemory
 from Folder_namedb import close_connection
-from AutoFileOrganiser import localtime_atStart
+# from AutoFileOrganiser import localtime_atStart
 
 folder_name_flag=None
-organise_lock = threading.Lock()
+
+def extract_zip(zip_file_path,extract_folder):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_folder)
+
 def set_folder_names():
     global images_folder_name   
     global project_folder_name  
@@ -65,43 +69,63 @@ def get_blendfile_folder():
         return None
     
 
-def organise(selected_folder):
-    src_folder=selected_folder
-    dest_folder=get_blendfile_folder()
+def organise():
+    src_folder=R"C:\zipextracttest"
+    dest_folder=R"C:\zipextracttest"
 
 
-    with organise_lock:
-        for file in os.listdir(src_folder):
-                
-                file_path=os.path.join(src_folder,file)
 
-                if os.path.getmtime(file_path)>=localtime_atStart: # yaha file ka time check karra agar program start hone se pehle koi file hogi toh uspe operation nai hoga
+    for file in os.listdir(src_folder):
+                                
+        file_path=os.path.join(src_folder,file)
 
-                    if os.path.isfile(file_path):
-                        extension=file.split('.')[-1] # file ka extension extract karra hai
+        if os.path.getmtime(file_path)>=localtime_atStart: # yaha file ka time check karra agar program start hone se pehle koi file hogi toh uspe operation nai hoga
 
-                        if extension in image_files:
+            if os.path.isfile(file_path):
+                extension=file.split('.')[-1] # file ka extension extract karra hai
 
-                            images_folder_path=os.path.join(dest_folder,images_folder_name)
-                            create_folder(images_folder_path)
-                            shutil.move(file_path,images_folder_path)
+                if extension in image_files:
 
-                        elif extension in project_files:
+                    images_folder_path=os.path.join(dest_folder,images_folder_name)
+                    create_folder(images_folder_path)
+                    shutil.move(file_path,images_folder_path)
 
-                            project_folder_path=os.path.join(dest_folder,project_folder_name)
-                            create_folder(project_folder_path)
-                            shutil.move(file_path,project_folder_path)
+                elif extension in project_files:
 
-                        elif extension in model_files:
+                    project_folder_path=os.path.join(dest_folder,project_folder_name)
+                    create_folder(project_folder_path)
+                    shutil.move(file_path,project_folder_path)
 
-                            model_folder_path=os.path.join(dest_folder,model_folder_name)
-                            create_folder(model_folder_path)
-                            shutil.move(file_path,model_folder_path)
+                elif extension in model_files:
+
+                    model_folder_path=os.path.join(dest_folder,model_folder_name)
+                    create_folder(model_folder_path)
+                    shutil.move(file_path,model_folder_path)
                         
-                        elif extension in mocap_files:
+                elif extension in mocap_files:
 
-                            mocap_folder_path=os.path.join(dest_folder,mocap_folder_name)
-                            create_folder(mocap_folder_path)
-                            shutil.move(file_path,mocap_folder_path)
-                else:
-                    break
+                    mocap_folder_path=os.path.join(dest_folder,mocap_folder_name)
+                    create_folder(mocap_folder_path)
+                    shutil.move(file_path,mocap_folder_path)
+
+                elif extension =='zip':
+                    extract_zip(file_path,dest_folder)
+
+
+        else:
+            break
+
+def unziptest():
+    src_folder=R"C:\zipextracttest"
+    dest_folder=R"C:\zipextracttest"
+    for file in os.listdir(src_folder):
+        file_path=os.path.join(src_folder,file)
+
+        if os.path.isfile(file_path):
+            extension=file.split('.')[-1]
+            if extension =='zip':
+                extract_zip(file_path,dest_folder)
+                print("Extraction complete")
+
+
+unziptest()
