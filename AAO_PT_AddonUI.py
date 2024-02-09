f8a535bf1d876c2d0d81c5c8c45d99c50dbb0734
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-09 17:50:40 +0530

    Update: Operator Description

--- AAO_PT_AddonUI.py
+++ AAO_PT_AddonUI.py
@@ -1,11 +1,11 @@
 import bpy 
 
 class OBJECT_PT_AssetManagerUI(bpy.types.Panel):
-    bl_label="Offline Organiser test"
+    bl_label="Real-time Asset Organiser"
     bl_idname="OBJECT_PT_addonui"
     bl_space_type="VIEW_3D"
     bl_region_type="UI"
-    bl_category="File Manager"
+    bl_category="Asset Organiser"
 
     def draw(self, context):
         layout=self.layout
@@ -15,4 +15,4 @@ class OBJECT_PT_AssetManagerUI(bpy.types.Panel):
 
         layout.prop(scene,"monitor_folder",text="")
 
-        layout.operator('object.printfoldername',text="Print folder name")
+        layout.operator('object.onclickorganise',text="Organise",text_ctxt='Organise downloaded files')
