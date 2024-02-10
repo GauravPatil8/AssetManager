697b614b418ab6824a5be732d81f9ef7d80426f5
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-10 15:18:12 +0530

    Update: Onclick Organise

--- AAO_UT_FileHandler.py
+++ AAO_UT_FileHandler.py
@@ -33,18 +33,33 @@ mocap_files     = ['bvh']
 
 material_files  = ['sbsar','spsm','spp','sbs']
 
+def is_blend_file_saved():
+    if bpy.data.filepath == "":
+        return False
+    else:
+        return True
+    
+def get_source_folder(flag):
+    if flag=='0':
+        return get_downloads_folder()
+    else:
+        if get_blendfile_folder()==None:
+            print("save toh karle")
+        else:
+            return get_blendfile_folder()
+     
+     
 
-
-def organise_zip(folder_path,dest_folder):
+def organise_zip(zip_file_path,destination_folder):
 
     flag=False                          
     extension_dictionary={}
     
     combined_list = model_files + project_files
 
-    subdirectory_name = os.path.splitext(os.path.basename(folder_path))[0]
+    subdirectory_name = os.path.splitext(os.path.basename(zip_file_path))[0]
 
-    with zipfile.ZipFile(folder_path, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         file_names=zip_ref.namelist()  
     
     blend_file_count=0
@@ -62,28 +77,30 @@ def organise_zip(folder_path,dest_folder):
                 gltf_file_count+=1
                       
         if extension in extension_dictionary:
-            if not  blend_file_count and gltf_file_count == 1:
-                flag=True
+            if blend_file_count and gltf_file_count == 1:
+                flag=False
+            else:
+                 flag=True
     
 
             
     if flag==True:
-        model_folder = os.path.join(dest_folder, "Model_Files")
+        model_folder = os.path.join(destination_folder, "Model_Files")
         create_folder(model_folder)
             
         subdirectory_path=os.path.join(model_folder,subdirectory_name)
         create_folder(subdirectory_path)
-        with zipfile.ZipFile(folder_path,'r') as zip_ref:
+        with zipfile.ZipFile(zip_file_path,'r') as zip_ref:
             zip_ref.extractall(subdirectory_path)
                         
     else:
                         
-        images_folder = os.path.join(dest_folder, "Image_Files")
+        images_folder = os.path.join(destination_folder, "Image_Files")
         create_folder(images_folder)
             
         subdirectory_path=os.path.join(images_folder,subdirectory_name)
         create_folder(subdirectory_path)
-        with zipfile.ZipFile(folder_path,'r') as zip_ref:
+        with zipfile.ZipFile(zip_file_path,'r') as zip_ref:
             zip_ref.extractall(subdirectory_path)
                
 
@@ -94,54 +111,46 @@ def create_folder(folder_path):
 
 def get_downloads_folder():
     home_directory=os.path.expanduser('~')
-
-    if os.name=='posix':
-        downloads_folder=os.path.join(home_directory,'Downloads')
-    
-    elif os.name=='nt':
-        downloads_folder=os.path.join(home_directory,'Downloads')
-    else:
-        raise OSError("Unsupported Operating System")
-    return downloads_folder
+    return os.path.join(home_directory,'Downloads')
 
 
-def organiser_utility(dest_folder,extension,file_path):
+def organiser_utility(destination_folder,extension,file_path):
     if extension in image_files:
 
-                    images_folder_path=os.path.join(dest_folder,images_folder_name)
+                    images_folder_path=os.path.join(destination_folder,images_folder_name)
                     create_folder(images_folder_path)
                     shutil.move(file_path,images_folder_path)
 
     elif extension in project_files:
 
-                    project_folder_path=os.path.join(dest_folder,project_folder_name)
+                    project_folder_path=os.path.join(destination_folder,project_folder_name)
                     create_folder(project_folder_path)
                     shutil.move(file_path,project_folder_path)
 
     elif extension in model_files:
 
-                    model_folder_path=os.path.join(dest_folder,model_folder_name)
+                    model_folder_path=os.path.join(destination_folder,model_folder_name)
                     create_folder(model_folder_path)
                     shutil.move(file_path,model_folder_path)
                         
     elif extension in mocap_files:
 
-                    mocap_folder_path=os.path.join(dest_folder,mocap_folder_name)
+                    mocap_folder_path=os.path.join(destination_folder,mocap_folder_name)
                     create_folder(mocap_folder_path)
                     shutil.move(file_path,mocap_folder_path)
 
     elif extension =='zip':
-        organise_zip(file_path,dest_folder)
+        organise_zip(file_path,destination_folder)
         os.remove(file_path)
 
 
     elif extension =='hdr':
-        hdri_folder_path=os.path.join(dest_folder,images_folder_name,"HDRI_Images")
+        hdri_folder_path=os.path.join(destination_folder,images_folder_name,"HDRI_Images")
         create_folder(hdri_folder_path)
         shutil.move(file_path,hdri_folder_path)
 
     elif extension in material_files:
-        material_folder_path=os.path.join(dest_folder,project_folder_name,"Materials") #project files ke jagah db name daal diyo
+        material_folder_path=os.path.join(destination_folder,project_folder_name,"Materials") #project files ke jagah db name daal diyo
         create_folder(material_folder_path)
         shutil.move(file_path,material_folder_path)
 
@@ -154,20 +163,18 @@ def get_blendfile_folder():
         return None
     
 
-def organise(src_folder,destination_folder,localtime_atStart):
-    
-    if destination_folder != None:
-        for file in os.listdir(src_folder):
-                                    
-            file_path=os.path.join(src_folder,file)
+def organise(source_folder_flag,destination_folder,localtime_at_Start):
+    print ("organise function tak poch gaya")#destination_folder,localtime_atStart):
+    # src_folder=R"c:\Users\GAURAV\Downloads\test_folder"
+    # destination_folder=R"c:\Users\GAURAV\Downloads\Temp"
+    for file in os.listdir(get_source_folder(source_folder_flag)):
+        # print(file)                         
+        file_path=os.path.join(get_source_folder(source_folder_flag),file)
 
-            if os.path.getmtime(file_path)>=localtime_atStart: # yaha file ka time check karra agar program start hone se pehle koi file hogi toh uspe operation nai hoga
+        if os.path.getmtime(file_path)>=localtime_at_Start: # yaha file ka time check karra agar program start hone se pehle koi file hogi toh uspe operation nai hoga
 
-                if os.path.isfile(file_path):
+            if os.path.isfile(file_path):
                     extension=file.split('.')[-1].lower() # file ka extension extract karra hai
+                    
+                    organiser_utility(destination_folder,extension,file_path)
                 
-                organiser_utility(destination_folder,extension,file_path)
-                
-            else:
-                break
-
