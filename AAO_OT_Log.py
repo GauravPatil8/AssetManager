c5ced799870a09021fcd46b07dd1d532f16f08c6
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-13 20:02:14 +0530

    Add: Database Connection

--- AAO_OT_Log.py
+++ AAO_OT_Log.py
@@ -1,4 +1,8 @@
 import bpy
+from AAO_DB_FolderNames import changelog_table_in_memory
+from AAO_DB_FolderNames import get_file_info_change_log
+
+memory_connection=changelog_table_in_memory()
 
 class OBJECT_OT_log(bpy.types.Operator):
     bl_idname = "object.log"
@@ -17,33 +21,13 @@ class OBJECT_OT_log_popup(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-
         
-        sample_data = [
-            ("Option 1", "This is the description for Option 1"),
-            ("Option 2", "This is the description for Option 2"),
-            ("Option 3", "This is the description for Option 3"),
-            ("Option 4", "This is the description for Option 4"),
-            ("Option 5", "This is the description for Option 5"),
-            ("Option 6", "This is the description for Option 6"),
-            ("Option 7", "This is the description for Option 7"),
-            ("Option 8", "This is the description for Option 8"),
-            ("Option 9", "This is the description for Option 9"),
-            ("Option 10", "This is the description for Option 10"),
-            ("Option 11", "This is the description for Option 11"),
-            ("Option 12", "This is the description for Option 12"),
-            ("Option 13", "This is the description for Option 13"),
-            ("Option 14", "This is the description for Option 14"),
-            ("Option 15", "This is the description for Option 15"),
-            ("Option 16", "This is the description for Option 16"),
-            ("Option 17", "This is the description for Option 17"),
-            ("Option 18", "This is the description for Option 18"),
-            ("Option 19", "This is the description for Option 19"),
-            ("Option 20", "This is the description for Option 20")
-        ]
+        file_data = []
+        file_info=get_file_info_change_log(memory_connection)
+        for row in file_info:
+            file_data.append(row)
 
-
-        for option, description in list(reversed(sample_data)):
+        for option, description in list(reversed(file_data)):
             layout.label(text=option,icon='MATERIAL')
             layout.label(text=description,icon='FOLDER_REDIRECT')
             layout.separator()
