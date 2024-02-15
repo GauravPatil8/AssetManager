7f2a25fd68d4e48bd7854261d6e5f2ade3c4d9e0
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-15 13:27:56 +0530

    Update: UI Layout

--- AAO_PT_AddonUI.py
+++ AAO_PT_AddonUI.py
@@ -1,33 +1,40 @@
-import bpy 
+import bpy
 
 class OBJECT_PT_AssetManagerUI(bpy.types.Panel):
-    bl_label="Real-time Asset Organiser"
-    bl_idname="OBJECT_PT_addonui"
-    bl_space_type="VIEW_3D"
-    bl_region_type="UI"
-    bl_category="Asset Organiser"
+    bl_label = "Real-time Asset Organiser"
+    bl_idname = "OBJECT_PT_addonui"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Asset Organiser"
 
     def draw(self, context):
-        layout=self.layout
-        scene=context.scene
+        layout = self.layout
+        scene = context.scene
 
-        layout.label(text="Select folder to monitor: ")
+        # Folder monitoring section
+        layout.label(text="Select folder to monitor:")
+        layout.prop(scene, "monitor_folder", text="")
 
-        layout.prop(scene,"monitor_folder",text="")
+        # Organizing type section
+        box1 = layout.box()
+        box1.label(text='Select organising type:')
+        box1.prop(scene, 'monitoring_type_prop', text='')
+        if context.scene.monitoring_type_prop == 'REALTIME':
+            box1.prop(scene, 'delay_time_prop', text='Delay Time')
+            box1.operator("object.realtimeops", text='Start')
+        else:
+            box1.operator('object.onclickorganise', text="Organise", text_ctxt='Organise downloaded files')
 
-        layout.operator('object.onclickorganise',text="Organise",text_ctxt='Organise downloaded files')
-
-        layout.separator()
-
+        # Change folder name section
         box = layout.box()
         row = box.row(align=True)
         row.prop(context.scene, "change_folder_name", text="Change Folder Name")
-
         if context.scene.change_folder_name:
             box.prop(context.scene, "folder_name", text="Select Folder Name")
-
             box.prop(context.scene, "custom_folder_name", text="Enter Folder Name")
-
             box.operator("object.updatefoldername", text="Set")
 
+        layout.separator()
+
+        # Log section
         layout.operator("object.log", text="Log")
