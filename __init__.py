ecf35c82fe61b1037949a81664c9331674c97467
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-16 17:08:38 +0530

    Update: Registration

--- __init__.py
+++ __init__.py
@@ -16,11 +16,12 @@
 import sys 
 import bpy
 import os
+
 script_path = os.path.abspath(__file__)
 package_path = os.path.dirname(script_path)
 sys.path.append(package_path)
 
-from AAO_OT_Onclick_Organise   import OBJECT_OT_Onclick_Organise
+from AAO_OT_Onclick_Organise   import OBJECT_OT_Onclick_Organise #classes
 from AAO_OT_Onclick_Organise   import ENUM_PROPS_monitor_folder
 from AAO_OT_Update_folder_name import OBJECT_OT_update_foldername
 from AAO_OT_Update_folder_name import ENUM_PROPS_Change_folder_name
@@ -32,10 +33,10 @@ from AAO_OT_Log                import OBJECT_OT_log
 from AAO_OT_Monitoring_Type    import OBJECT_OT_monitor_type
 from AAO_OT_Monitoring_Type    import ENUM_PROPS_delay_time
 from AAO_OT_Monitoring_Type    import ENUM_PROPS_monitoring_type
-from AAO_OT_Onclick_Organise   import on_start
+from AAO_OT_Onclick_Organise   import on_start                   #functions
 from AAO_OT_Onclick_Organise   import blender_folder_on_saved
 from AAO_DB_FolderNames        import close_connection
-from AAO_UT_FileHandler        import memory_connection
+from AAO_UT_FileHandler        import memory_connection         #variables
 from AAO_UT_FileHandler        import database_connection
 
 
@@ -51,21 +52,22 @@ classes=(OBJECT_OT_Onclick_Organise,OBJECT_PT_AssetManagerUI,OBJECT_OT_update_fo
 
 
 def register():
-   
+    
     bpy.app.handlers.load_post.append(on_start)
     bpy.app.handlers.save_post.append(blender_folder_on_saved)
     for kls in classes:
             bpy.utils.register_class(kls)
     bpy.app.handlers.persistent.append(on_exit)
 
+
 def unregister():
-    
+
     bpy.app.handlers.load_post.remove(on_start)
     bpy.app.handlers.save_post.remove(blender_folder_on_saved)
     for kls in reversed(classes):
         bpy.utils.unregister_class(kls)
     del bpy.types.Scene.custom_folder_name
     bpy.app.handlers.persistent.remove(on_exit)
-
+  
 register()
 
