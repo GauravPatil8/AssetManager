e4092d08e31615aad0f64c72fbf61e1765916458
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-02-17 22:46:08 +0530

    Update: 'on_exit' Handler

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@
 import sys 
 import bpy
 import os
-
+import atexit
 script_path = os.path.abspath(__file__)
 package_path = os.path.dirname(script_path)
 sys.path.append(package_path)
@@ -36,38 +36,44 @@ from AAO_OT_Monitoring_Type    import ENUM_PROPS_monitoring_type
 from AAO_OT_Onclick_Organise   import on_start                   #functions
 from AAO_OT_Onclick_Organise   import blender_folder_on_saved
 from AAO_DB_FolderNames        import close_connection
-from AAO_UT_FileHandler        import memory_connection         #variables
+# from AAO_UT_FileHandler        import memory_connection         #variables
 from AAO_UT_FileHandler        import database_connection
 
 
 
-def on_exit(dummy):
-    close_connection(memory_connection)
-    close_connection(database_connection)
+def on_exit():
+    print("Sab KHatam hogaya")
+    # close_connection(memory_connection)
+    # close_connection(database_connection)
      
      
 
 classes=(OBJECT_OT_Onclick_Organise,OBJECT_PT_AssetManagerUI,OBJECT_OT_update_foldername,OBJECT_OT_log,OBJECT_OT_log_popup,OBJECT_OT_monitor_type,ENUM_PROPS_Change_folder_name,ENUM_PROPS_Folder_name,ENUM_PROPS_delay_time,ENUM_PROPS_monitoring_type, ENUM_PROPS_custom_folder_name,ENUM_PROPS_monitor_folder) 
 
-
-
-def register():
-    
+def register_handlers():
     bpy.app.handlers.load_post.append(on_start)
     bpy.app.handlers.save_post.append(blender_folder_on_saved)
+    atexit.register(on_exit)
+
+def unregister_handlers():
+    bpy.app.handlers.load_post.remove(on_start)
+    bpy.app.handlers.save_post.remove(blender_folder_on_saved)
+    atexit.unregister(on_exit)
+     
+
+def register():    
+    register_handlers()
     for kls in classes:
             bpy.utils.register_class(kls)
-    bpy.app.handlers.persistent.append(on_exit)
+    
 
 
 def unregister():
-
-    bpy.app.handlers.load_post.remove(on_start)
-    bpy.app.handlers.save_post.remove(blender_folder_on_saved)
+    unregister_handlers()
     for kls in reversed(classes):
         bpy.utils.unregister_class(kls)
     del bpy.types.Scene.custom_folder_name
-    bpy.app.handlers.persistent.remove(on_exit)
+    
   
 register()
 
