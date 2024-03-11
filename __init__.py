b7d5c858eb997cc25289fd1f8c03a16da87d165e
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-03-11 21:41:13 +0530

    Update: Registration

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 #     "name": "Realtime Asset Manager",
 #     "blender": (4, 0, 2),
 #     "category": "System", 
-#     "author": "Swami",
+#     "author": "Gaurav",
 #     "version": (0, 0, 1),
 #     "location": "View3D > UI> Asset Organiser",
 #     "description": "Real-time Asset Organizer for 3D Artists is a powerful Blender addon designed to streamline and enhance the workflow of 3D artists by providing a dynamic and efficient asset management system.",
@@ -28,17 +28,16 @@ from AAO_OT_Update_folder_name import Bool_PROPS_Change_folder_name
 from AAO_OT_Update_folder_name import ENUM_PROPS_Folder_name
 from AAO_OT_Update_folder_name import STRING_PROPS_custom_folder_name
 from AAO_PT_AddonUI            import OBJECT_PT_AssetManagerUI
-from AAO_PT_AddonUI            import OBJECT_PT_preset_creator
+from AAO_OT_Preset_creator     import OBJECT_PT_preset_creator
 from AAO_OT_Log                import OBJECT_OT_log_popup
 from AAO_OT_Log                import OBJECT_OT_log
 from AAO_OT_Monitoring_Type    import OBJECT_OT_monitor_type
 from AAO_OT_Monitoring_Type    import ENUM_PROPS_delay_time
 from AAO_OT_Monitoring_Type    import ENUM_PROPS_monitoring_type
 from AAO_OT_Preset_creator     import ENUM_PROPS_folder_presets
-from AAO_OT_Preset_creator     import Bool_PROPS_Create_preset
 from AAO_OT_Preset_creator     import STRING_PROPS_preset_analysis_folder
-from AAO_OT_Preset_creator     import OBJECT_OT_save_preset
-from AAO_OT_Preset_creator     import OBJECT_OT_OpenFolderOperator
+from AAO_OT_Preset_creator     import OPEN_FOLDER_OT_OpenFolder
+from AAO_OT_Preset_creator     import ENUM_PROPS_Tags
 from AAO_OT_Onclick_Organise   import on_start                   #functions
 from AAO_OT_Onclick_Organise   import blender_folder_on_saved
 from AAO_DB_FolderNames        import close_connection         
@@ -51,30 +50,31 @@ def on_exit():
      
      
 
-classes=(OBJECT_OT_Onclick_Organise,OBJECT_PT_AssetManagerUI,OBJECT_PT_preset_creator,OBJECT_OT_update_foldername,OBJECT_OT_log,OBJECT_OT_log_popup,OBJECT_OT_monitor_type,Bool_PROPS_Change_folder_name,ENUM_PROPS_Folder_name,ENUM_PROPS_delay_time,ENUM_PROPS_monitoring_type,STRING_PROPS_custom_folder_name,ENUM_PROPS_monitor_folder,ENUM_PROPS_folder_presets,Bool_PROPS_Create_preset,OBJECT_OT_save_preset,STRING_PROPS_preset_analysis_folder,OBJECT_OT_OpenFolderOperator) 
+classes=(ENUM_PROPS_monitor_folder,ENUM_PROPS_Tags,ENUM_PROPS_Folder_name,ENUM_PROPS_delay_time,ENUM_PROPS_monitoring_type,ENUM_PROPS_folder_presets,STRING_PROPS_preset_analysis_folder,STRING_PROPS_custom_folder_name,Bool_PROPS_Change_folder_name,OBJECT_OT_Onclick_Organise,OBJECT_OT_update_foldername,OBJECT_OT_log_popup,OBJECT_OT_log,OBJECT_OT_monitor_type,OPEN_FOLDER_OT_OpenFolder,OBJECT_PT_AssetManagerUI,OBJECT_PT_preset_creator) 
 
 
 def register(): 
-    bpy.app.handlers.load_post.append(on_start)
     
-    atexit.register(on_exit)   
-    bpy.app.handlers.save_post.append(blender_folder_on_saved)
     for kls in classes:
             bpy.utils.register_class(kls)
+    bpy.types.Scene.enum_properties = bpy.props.CollectionProperty(type=ENUM_PROPS_Tags)
+    bpy.app.handlers.load_post.append(on_start) 
+    atexit.register(on_exit)   
+    bpy.app.handlers.save_post.append(blender_folder_on_saved)
+    
     
     
 
 
 def unregister():
-    bpy.app.handlers.save_post.remove(blender_folder_on_saved)
-    
+
     for kls in reversed(classes):
         bpy.utils.unregister_class(kls)
     del bpy.types.Scene.custom_folder_name
     bpy.app.handlers.load_post.remove(on_start)
-    
+    bpy.app.handlers.save_post.remove(blender_folder_on_saved)
     atexit.unregister(on_exit)
+    del bpy.types.Scene.enum_properties
     
   
 register()
-
