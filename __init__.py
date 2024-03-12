427d92f0b5daf545251cadea341acb210ee4b07f
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-03-12 18:51:13 +0530

    Update: registration

--- __init__.py
+++ __init__.py
@@ -29,15 +29,17 @@ from AAO_OT_Update_folder_name import ENUM_PROPS_Folder_name
 from AAO_OT_Update_folder_name import STRING_PROPS_custom_folder_name
 from AAO_PT_AddonUI            import OBJECT_PT_AssetManagerUI
 from AAO_OT_Preset_creator     import OBJECT_PT_preset_creator
-from AAO_OT_Log                import OBJECT_OT_log_popup
-from AAO_OT_Log                import OBJECT_OT_log
-from AAO_OT_Monitoring_Type    import OBJECT_OT_monitor_type
-from AAO_OT_Monitoring_Type    import ENUM_PROPS_delay_time
 from AAO_OT_Monitoring_Type    import ENUM_PROPS_monitoring_type
+from AAO_OT_Monitoring_Type    import ENUM_PROPS_delay_time
 from AAO_OT_Preset_creator     import ENUM_PROPS_folder_presets
 from AAO_OT_Preset_creator     import STRING_PROPS_preset_analysis_folder
 from AAO_OT_Preset_creator     import OPEN_FOLDER_OT_OpenFolder
 from AAO_OT_Preset_creator     import ENUM_PROPS_Tags
+from AAO_OT_Preset_creator     import OBJECT_OT_save_preset
+from AAO_OT_Preset_creator     import OBJECT_OT_update_preset_list
+from AAO_OT_Log                import OBJECT_OT_log_popup
+from AAO_OT_Log                import OBJECT_OT_log
+from AAO_OT_Monitoring_Type    import OBJECT_OT_monitor_type
 from AAO_OT_Onclick_Organise   import on_start                   #functions
 from AAO_OT_Onclick_Organise   import blender_folder_on_saved
 from AAO_DB_FolderNames        import close_connection         
@@ -47,14 +49,12 @@ from AAO_UT_FileHandler        import database_connection #variables
 
 def on_exit():
     close_connection(database_connection)
-     
-     
 
-classes=(ENUM_PROPS_monitor_folder,ENUM_PROPS_Tags,ENUM_PROPS_Folder_name,ENUM_PROPS_delay_time,ENUM_PROPS_monitoring_type,ENUM_PROPS_folder_presets,STRING_PROPS_preset_analysis_folder,STRING_PROPS_custom_folder_name,Bool_PROPS_Change_folder_name,OBJECT_OT_Onclick_Organise,OBJECT_OT_update_foldername,OBJECT_OT_log_popup,OBJECT_OT_log,OBJECT_OT_monitor_type,OPEN_FOLDER_OT_OpenFolder,OBJECT_PT_AssetManagerUI,OBJECT_PT_preset_creator) 
+classes=(ENUM_PROPS_monitor_folder,ENUM_PROPS_delay_time,ENUM_PROPS_Tags,ENUM_PROPS_Folder_name,ENUM_PROPS_monitoring_type,ENUM_PROPS_folder_presets,STRING_PROPS_preset_analysis_folder,STRING_PROPS_custom_folder_name,Bool_PROPS_Change_folder_name,OBJECT_OT_update_preset_list,OBJECT_OT_Onclick_Organise,OBJECT_OT_update_foldername,OBJECT_OT_save_preset,OBJECT_OT_log_popup,OBJECT_OT_log,OBJECT_OT_monitor_type,OPEN_FOLDER_OT_OpenFolder,OBJECT_PT_AssetManagerUI,OBJECT_PT_preset_creator) 
 
 
 def register(): 
-    
+   
     for kls in classes:
             bpy.utils.register_class(kls)
     bpy.types.Scene.enum_properties = bpy.props.CollectionProperty(type=ENUM_PROPS_Tags)
@@ -68,7 +68,7 @@ def register():
 
 def unregister():
 
-    for kls in reversed(classes):
+    for kls in classes:
         bpy.utils.unregister_class(kls)
     del bpy.types.Scene.custom_folder_name
     bpy.app.handlers.load_post.remove(on_start)
