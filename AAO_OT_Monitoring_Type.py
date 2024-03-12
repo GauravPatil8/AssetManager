21ffcd130cbf812163541f0ebc10a5a8e9cc51b5
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-03-12 14:14:01 +0530

    Add: Flag Checks

--- AAO_OT_Monitoring_Type.py
+++ AAO_OT_Monitoring_Type.py
@@ -8,7 +8,13 @@ from AAO_OT_Onclick_Organise import get_downloads_folder
 from AAO_OT_Onclick_Organise import get_blendfile_folder
 from AAO_OT_Onclick_Organise import create_folder
 from AAO_OT_Onclick_Organise import local_time_at_start
+
+stop_event = threading.Event()
+thread_flag=False
+report_flag=False
 blender_folder=None
+
+
 def get_blender_folder_path():
     global blender_folder
     old_blender_folder=get_blendfile_folder()
@@ -16,32 +22,35 @@ def get_blender_folder_path():
         one_blender_folder=os.path.dirname(old_blender_folder)
         blender_folder=os.path.dirname(one_blender_folder)
 
-stop_event = threading.Event()
-threads_counter=0
-report_flag=False
-def monitoring_type_prop_update_handler(self, context):
-    if self.monitoring_type_prop == 'on-click':
+
+def monitoring_type_prop_update_handler(self,context):
+    global thread_flag
+    global report_flag
+    
+    if self.monitoring_type_prop == 'ONCLICKOPERATOR':
         stop_event.set() 
+        thread_flag=False
+        report_flag=False
     
  
 class ENUM_PROPS_monitoring_type(bpy.types.PropertyGroup):
     bpy.types.Scene.monitoring_type_prop = bpy.props.EnumProperty(
             items=[
                 ('REALTIME','Real-time','Organises folder in real-time'),
-                ('on-click','On-click','Organises folder on click'),
+                ('ONCLICKOPERATOR','On-click','Organises folder on click'),
             ],
-            default='on-click',
-            update=monitoring_type_prop_update_handler
+            default='ONCLICKOPERATOR',
+            update=monitoring_type_prop_update_handler,
         )
 
 class ENUM_PROPS_delay_time(bpy.types.PropertyGroup):
-    bpy.types.Scene.delay_time_prop = bpy.props.EnumProperty(
+    bpy.types.Scene.delay_time_prop =bpy.props.EnumProperty(
         items=[
             ('THREE','3 Seconds','Orgnises folder every three seconds'),
             ('SEVEN','7 Seconds','Orgnises folder every seven seconds'),
             ('THREE','10 Seconds','Orgnises folder every ten seconds')
         ],
-        default='THREE'
+        default='THREE',
     )
 
 def realtime_monitoring(self,context,stop_event):
@@ -69,11 +78,13 @@ def realtime_monitoring(self,context,stop_event):
                     context.scene.monitor_folder='DOWNLOADS'    
                 
             if context.scene.delay_time_prop == 'THREE':
-                    time.sleep(3)
+                time.sleep(3)
+
             elif context.scene.delay_time_prop == 'SEVEN': 
-                    time.sleep(7)
+                time.sleep(7)
+
             else:
-                    time.sleep(10)
+                time.sleep(10)
         
     
             
@@ -86,7 +97,7 @@ class OBJECT_OT_monitor_type(bpy.types.Operator):
     
 
     def execute(self, context):
-        global threads_counter
+        global thread_flag
         global stop_event
         stop_event.clear()
        
@@ -94,25 +105,20 @@ class OBJECT_OT_monitor_type(bpy.types.Operator):
         if context.scene.monitor_folder !="DOWNLOADS":
             if is_blend_file_saved():
                 self.report({'INFO'},"Real-time monitoring has started")
-                if threads_counter==0:
-                    threads_counter+=1
+                if thread_flag==False:
+                    thread_flag=True
                     threading.Thread(target=realtime_monitoring,daemon=True, args=(self,context,stop_event)).start()
                     self.report({'INFO'},"Real-time monitoring has started")
             else:
                 self.report({'ERROR'},"Blender file has not been saved. Please save your Blender file before utilizing this option.")
                 context.scene.monitor_folder="DOWNLOADS"
         else:
-            if threads_counter==0:
-                threads_counter+=1
+            if thread_flag==False:
                 threading.Thread(target=realtime_monitoring,daemon=True, args=(self,context,stop_event)).start()
                 self.report({'INFO'},"Real-time monitoring has started")
+                thread_flag=True
                 
         if report_flag==True:
              self.report({'WARNING'},"Real-time monitoring has already Started")
         
         return {'FINISHED'}
-
-
-
-
-
