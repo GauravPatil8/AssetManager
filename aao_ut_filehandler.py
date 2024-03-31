14a426d7b174fb8abc51d86807dc32848c368be6
gauravpatil-8 <patilgauravpradeep@gmail.com>
2024-03-31 22:37:52 +0530

    Update: zip function

--- aao_ut_filehandler.py
+++ aao_ut_filehandler.py
@@ -4,6 +4,7 @@ import bpy
 import zipfile
 import json
 import sys
+import datetime
 from aao_ot_log import file_data
 
 def get_package_path():
@@ -32,19 +33,38 @@ temporary_folder_name='Temporary asset folder'
 
 project_files = ['max', '3ds', 'blend', 'c4d', 'bgeo', 'geo','zpr','ma','mb','skp','3dm','sldprt','sldasm','zbp','hip']
 
-model_files = ['obj', 'fbx', 'usdz', 'dae',
-               'usd*', 'ply', 'glb', 'gltf', 'x3d','ztl','stl','abc']
+model_files = ['obj', 'fbx', 'usdz', 'dae','usd*', 'ply', 'glb', 'gltf', 'x3d','ztl','stl','abc']
 
-image_files = ['png', 'jpg', 'jpeg', 'exr', 'tiff', 'webp',
-               'gif', 'psd', 'indd', 'raw', 'svg', 'ai', 'tif','avif','hdr']
+image_files = ['png', 'jpg', 'jpeg', 'exr', 'tiff', 'webp','gif', 'psd', 'indd', 'raw', 'svg', 'ai', 'tif','avif','hdr']
 
 material_files = ['sbsar', 'spsm', 'spp', 'sbs']
 
 video_files = ['mov', 'mp4', 'mkv', 'avi', 'wmv', 'avchd', 'webm', 'flv']
 
-audio_files = ['wav', 'mp3', 'flac', 'ogg', 'm3u', 'acc',
-               'wma', 'wav', 'midi', 'aif', 'm4a', 'mpa', 'pls']
+audio_files = ['wav', 'mp3', 'flac', 'ogg', 'm3u', 'acc','wma', 'wav', 'midi', 'aif', 'm4a', 'mpa', 'pls']
 
+fType_dict={}
+
+def dictionary_constructor():
+    '''
+    name               index
+    -------------------------
+    project_files      : 0 
+    model_files        : 1   
+    image_files        : 2  
+    material_files     : 3
+    video_files        : 4
+    audio_files        : 5
+    
+    '''
+    global fType_dict
+    fType_list=[project_files,model_files,image_files,material_files,video_files,audio_files]
+    fType_names=[project_folder_destination,model_folder_destination,images_folder_destination,material_folder_destination,video_folder_destination,audio_folder_destination]
+    
+    for index,extension_list in enumerate(fType_list):
+        fType_dict[tuple(extension_list)]= fType_names[index]
+
+dictionary_constructor()
 def update_recent_file_path(old_path, new_path):
     
     config_directory = bpy.utils.user_resource('CONFIG')
@@ -63,8 +83,6 @@ def update_recent_file_path(old_path, new_path):
         with open(recent_files_path, "w") as file:
             file.write("\n".join(modified_lines))
 
-        
-   
 
 def get_blendfile_folder():
     bfp = bpy.data.filepath
@@ -73,8 +91,6 @@ def get_blendfile_folder():
     else:
         return None
 
-
-
 def return_projectfile_name():
     scene = bpy.context.scene
     
@@ -82,9 +98,9 @@ def return_projectfile_name():
         preset_path=os.path.join(file_folder_path,scene.folder_presets+'.json')
         with open(preset_path) as f:
             f_names=json.load(f)
-        return f_names.get('PROJECT',"Project_Files")
+        return f_names.get('PROJECT',project_folder_destination)
     else:
-        return "Project_Files"
+        return project_folder_destination
         
 
 def path_constructor():
@@ -119,6 +135,7 @@ def path_constructor():
         material_folder_destination = "Material_files"
         video_folder_destination = "Video_files"
         audio_folder_destination = "Audio_files"
+    dictionary_constructor()
     
         
 def blender_folder_on_saved(dummy):
@@ -167,19 +184,16 @@ def blender_folder_on_saved(dummy):
         update_recent_file_path(old_file_path,new_file_path)
         save_flag =True
 
-
 def log_info(file_name, file_path):
     info_list = [file_name, file_path]
     log_tuple = tuple(info_list)
     file_data.append(log_tuple)
-
 
 def is_blend_file_saved():
     if bpy.data.filepath == "":
         return False
     else:
         return True
-
 
 def get_source_folder(flag):
     if flag == '0':
@@ -190,7 +204,6 @@ def get_source_folder(flag):
         newpath_1 = os.path.dirname(path)
         newpath_2 = os.path.dirname(newpath_1)
         return newpath_2
-
 
 def reload_image_textures(target_folder):
     for material in bpy.data.materials:
@@ -207,68 +220,86 @@ def reload_image_textures(target_folder):
                             image_texture.filepath = bpy.path.abspath(new_filepath)
                             image_texture.reload()
                             
-            
-
-
 def organise_zip(zip_file_path, destination_folder, file_name):
-
-    flag = False
-    extension_dictionary = {}
+    
+    video_count=0
+    audio_count=0
+    image_count=0
+    flag_dictionary={'model_flag': False,'Video_flag':False,'Audio_flag':False,'image_flag':False}
 
     combined_list = model_files + project_files
 
-    subdirectory_name = os.path.splitext(os.path.basename(zip_file_path))[0]
-
+    subdirectory_name = os.path.splitext(os.path.basename(zip_file_path))[0]+' '+str(datetime.datetime.now())
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         file_names = zip_ref.namelist()
 
-    blend_file_count = 0
-    gltf_file_count = 0
+    
     for file in file_names:
         extension = file.split('.')[-1].lower()
         if extension in combined_list:
-            extension_dictionary[extension] = extension_dictionary.get(
-                extension, 0)+1
-
-            # handling polyhaven files
-
-            if extension == 'blend':
-                blend_file_count += 1
-            elif extension == 'gltf':
-                gltf_file_count += 1
-
-        if extension in extension_dictionary:
-            if blend_file_count and gltf_file_count == 1:
-                flag = False
+            flag_dictionary['model_flag'] = True
+        elif extension in video_files:
+            video_count+=1
+            flag_dictionary['Video_flag'] = True
+        elif extension in audio_files:
+            audio_count+=1
+            flag_dictionary['Audio_flag'] = True
+        elif extension in image_files:
+            image_count+=1
+            flag_dictionary['image_flag'] = True
+    if flag_dictionary['model_flag']!=True:
+        num_dict={'Video_flag':video_count,'Audio_flag':audio_count,'image_flag':image_count}
+        largest_num = max(num_dict.values())
+        variable_with_max = [name for name, value in num_dict.items() if value == largest_num][0]
+        for key in flag_dictionary:
+            if key==variable_with_max:
+                flag_dictionary[key]=True
             else:
-                flag = True
+                flag_dictionary[key]=False
 
-    if flag == True:
+    if flag_dictionary['model_flag'] == True:
         model_folder = os.path.join(destination_folder, model_folder_destination)
         create_folder(model_folder)
-
+        
         subdirectory_path = os.path.join(model_folder, subdirectory_name)
         create_folder(subdirectory_path)
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
             zip_ref.extractall(subdirectory_path)
         log_info(file_name, model_folder)
 
-    else:
+    elif flag_dictionary['Video_flag'] == True:
 
-        images_folder = os.path.join(destination_folder, images_folder_destination)
-        create_folder(images_folder)
+        video_folder = os.path.join(destination_folder, video_folder_destination)
+        create_folder(video_folder)
 
-        subdirectory_path = os.path.join(images_folder, subdirectory_name)
+        subdirectory_path = os.path.join(video_folder, subdirectory_name)
         create_folder(subdirectory_path)
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
             zip_ref.extractall(subdirectory_path)
-        log_info(file_name, images_folder)
+        log_info(file_name, video_folder)
+    elif flag_dictionary['Audio_flag']==True:
+        audio_folder = os.path.join(destination_folder, audio_folder_destination)
+        create_folder(audio_folder)
+
+        subdirectory_path = os.path.join(audio_folder, subdirectory_name)
+        create_folder(subdirectory_path)
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(subdirectory_path)
+        log_info(file_name, audio_folder)
+    elif flag_dictionary['image_flag']==True:
+        image_folder = os.path.join(destination_folder, images_folder_destination)
+        create_folder(image_folder)
+
+        subdirectory_path = os.path.join(image_folder, subdirectory_name)
+        create_folder(subdirectory_path)
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(subdirectory_path)
+        log_info(file_name, image_folder)
 
 
 def create_folder(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-
 
 def get_downloads_folder():
 
@@ -281,7 +312,6 @@ def get_downloads_folder():
     else:
         return os.path.join(os.path.expanduser('~'), 'Downloads')
 
-
 def duplicate_handler(file_path, file, folder_path, extension):
     global filecount
     if os.path.exists(os.path.join(folder_path, file)):
@@ -293,51 +323,17 @@ def duplicate_handler(file_path, file, folder_path, extension):
         shutil.move(file_path, folder_path)
     filecount += 1
 
-
 def organiser_utility(destination_folder, extension, file_path, file):
-
-    if extension in image_files:
-        images_folder_path = os.path.join(destination_folder, images_folder_destination)
-        create_folder(images_folder_path)
-        duplicate_handler(file_path, file, images_folder_path, extension)
-        log_info(file, images_folder_path)
-
-    elif extension in project_files:
-        project_folder_path = os.path.join(destination_folder, project_folder_destination)
-        create_folder(project_folder_path)
-        duplicate_handler(file_path, file, project_folder_path, extension)
-        log_info(file, project_folder_path)
-
-    elif extension in model_files:
-        model_folder_path = os.path.join(destination_folder, model_folder_destination)
-        create_folder(model_folder_path)
-        duplicate_handler(file_path, file, model_folder_path, extension)
-        log_info(file, model_folder_path)
-
-
-    elif extension == 'zip':
+    if extension!='zip':
+        for key in fType_dict.keys():
+            if extension in key:
+                folder_path = os.path.join(destination_folder, fType_dict[key])
+                create_folder(folder_path)
+                duplicate_handler(file_path, file, folder_path, extension)
+                log_info(file, folder_path)
+    else:
         organise_zip(file_path, destination_folder, file)
         os.remove(file_path)
-
-    elif extension in material_files:
-        material_folder_path = os.path.join(destination_folder, project_folder_destination, material_folder_destination)
-        create_folder(material_folder_path)
-        duplicate_handler(file_path, file, material_folder_path, extension)
-        log_info(file, material_folder_path)
-
-    elif extension in video_files:
-        video_folder_path = os.path.join(destination_folder, project_folder_destination, video_folder_destination)
-        create_folder(video_folder_path)
-        duplicate_handler(file_path, file, video_folder_path, extension)
-        log_info(file, video_folder_path)
-
-    elif extension in audio_files:
-        audio_folder_path = os.path.join(destination_folder, project_folder_destination, audio_folder_destination)
-        create_folder(audio_folder_path)
-        duplicate_handler(file_path, file, audio_folder_path, extension)
-        log_info(file, audio_folder_path)
-
-
 
 def organise(source_folder_flag, destination_folder, localtime_at_Start):
     source_folder=get_source_folder(source_folder_flag)
